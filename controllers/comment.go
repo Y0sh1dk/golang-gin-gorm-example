03ea618982e51866239728b5cf02135d9ec0dabc
Yosiah de Koeyer <yosiahdekoeyer@gmail.com>
2022-01-15 13:11:54 +1100

    Initial comment controller and comment queries

---
+++ controllers/comment.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func (s *Server) GetComments(c *gin.Context) {
+
+}
+
+func (s *Server) GetCommentByID(c *gin.Context) {
+
+}
+
+func (s *Server) CreateComment(c *gin.Context) {
+
+}
+
+func (s *Server) UpdateCommentByID(c *gin.Context) {
+
+}
+
+func (s *Server) DeleteCommentByID(c *gin.Context) {
+
+}
--- models/comment.go
+++ models/comment.go
@@ -22,10 +22,16 @@ func (c *Comment) Prepare() {
 }
 
 func GetComments(db *gorm.DB, comments *[]Comment) error {
+	if err := db.Find(comments).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
-func GetCommentByID(db *gorm.DB, comment *Comment) error {
+func GetCommentByID(db *gorm.DB, comment *Comment, id string) error {
+	if err := db.Where("id = ?", id).Find(comment).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -34,9 +40,19 @@ func CreateComment(db *gorm.DB, comment *Comment) error {
 }
 
 func UpdateComment(db *gorm.DB, comment *Comment) error {
+	comment.UpdatedAt = time.Now()
+	if err := db.Model(&comment).Updates(comment).Error; err != nil {
+		return err
+	}
 	return nil
 }
 
-func DeleteComment(db *gorm.DB, comment *Comment) error {
+func DeleteComment(db *gorm.DB, comment *Comment, id string) error {
+	if err := GetCommentByID(db, comment, id); err != nil {
+		return err
+	}
+	if err := db.Delete(comment).Error; err != nil {
+		return err
+	}
 	return nil
 }
