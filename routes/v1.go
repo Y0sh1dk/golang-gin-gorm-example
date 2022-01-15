a5132e8a50a1cdffe49218c613c22f56c51e75c7
Yosiah de Koeyer <yosiahdekoeyer@gmail.com>
2022-01-16 00:51:02 +1100

    Add comment routes

--- routes/v1.go
+++ routes/v1.go
@@ -19,4 +19,10 @@ func v1(server *controllers.Server) {
 	v1.GET("/posts/:id", server.GetPostByID)
 	v1.DELETE("/posts/:id", server.DeletePostByID)
 	v1.PUT("/posts/:id", server.UpdatePostByID)
+
+	v1.GET("/comments", server.GetComments)
+	v1.POST("/comments", server.CreateComment)
+	v1.GET("/comments/:id", server.GetCommentByID)
+	v1.DELETE("/comments/:id", server.DeleteCommentByID)
+	v1.PUT("/comments/:id", server.UpdateCommentByID)
 }
