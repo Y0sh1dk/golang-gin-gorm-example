453ed9d0d5201fb2302ae93af92280e0c2ed215e
Yosiah de Koeyer <yosiahdekoeyer@gmail.com>
2022-01-15 12:55:44 +1100

    Cleanup

--- routes/v1.go
+++ routes/v1.go
@@ -11,7 +11,6 @@ func v1(server *controllers.Server) {
 	v1 := server.Router.Group("/v1")
 
 	v1.GET("/ping", func(c *gin.Context) {
-		// c.JSON(http.StatusOK, gin.H{"Status": "ok"})
 		c.String(http.StatusOK, "pong")
 	})
 
